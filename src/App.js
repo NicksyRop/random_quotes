b86730f2e6ecd83dd85b37e2752f315b77fa77f4
NicksyRop <nicksonkipkorir25@gmail.com>
2022-01-21 10:33:10 +0300

    installed axios to fetch quotes from api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,37 @@
 import React from 'react';
+import axios from 'axios';
 
 import './App.css';
 
 class App extends React.Component{
+
+
+  state = {
+
+    advice : ''
+  }
+  componentDidMount(){
+
+    this.fetchAdvice()
+  }
+  fetchAdvice(){
+
+    axios.get("https://api.adviceslip.com/advice")
+    .then( (response) => {
+
+      const { advice } = response.data.slip
+     this.setState({
+
+      advice : advice
+
+     })
+    })
+    .catch( (error) => {
+
+      console.error(error);
+
+    })
+  }
 
 
   render(){
